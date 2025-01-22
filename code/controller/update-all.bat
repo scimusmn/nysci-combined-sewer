f60436d40ddc0eaf18a2464e11baff136c624a03
sanine <sanine.not@pm.me>
2025-01-22 09:15:39 -0600

    fix update-all.bat

--- code/controller/update-all.bat
+++ code/controller/update-all.bat
@@ -1,12 +1,13 @@
-cd controller-far-left
-update.bat
-cd ..
-cd controller-far-right
-update.bat
-cd ..
-cd controller-center-left
-update.bat
-cd ..
-cd controller-center-right
-update.bat
-cd ..
+@ECHO OFF
+pushd controller-far-left
+call update.bat
+popd
+pushd controller-far-right
+call update.bat
+popd
+pushd controller-center-left
+call update.bat
+popd
+pushd controller-center-right
+call update.bat
+popd
